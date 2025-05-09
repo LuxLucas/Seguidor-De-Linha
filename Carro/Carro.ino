f3d0e85fa76d27838f048aafce4c542a39e8c871
Lucas Flôres <124315388+LuxLucas@users.noreply.github.com>
2025-05-09 14:42:57 -0300

    Add files via upload

---
+++ Carro/Carro.ino
@@ -0,0 +1,32 @@
+#include "MotorCC.h"
+#include "SensorInfravermelho.h"
+MotorCC motorA(4,5,3);
+MotorCC motorB(9,10,11);
+SensorInfravermelho sensorA(2);
+SensorInfravermelho sensorB(12);
+void setup() {
+    Serial.begin(9600);
+    motorA.girarAntiHorario(0);
+    motorB.girarAntiHorario(0);
+}
+
+void loop() {
+
+ 
+  if(sensorA.isReflect() && !sensorB.isReflect()){
+    motorA.girarHorario(100);
+    motorB.girarAntiHorario(100);
+  }else if(!sensorA.isReflect() && sensorB.isReflect()){
+    motorA.girarAntiHorario(100);
+    motorB.girarHorario(100);
+  }else{
+    motorA.girarHorario(100);
+    motorB.girarHorario(100);
+  }
+ 
+  /*Serial.println(sensorA.isReflect());
+   Serial.println(sensorB.isReflect());
+    Serial.println("-----------");
+    delay(1000);
+    */
+}
